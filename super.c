a7cff33dfd020e4f686b599afafeb5da9aab6497
HongweiQin <460897705@qq.com>
2019-10-10 20:54:11 +0800

    Can use directories. Can create regular files. Can read/write file. Fsync has issues.

--- super.c
+++ super.c
@@ -18,6 +18,10 @@
 #include <linux/quotaops.h>
 #include <linux/sysfs.h>
 #include <linux/quota.h>
+#include <linux/pagevec.h>
+#include <linux/mm_inline.h>
+#include <linux/uio.h>
+#include <linux/sched/signal.h>
 
 /* Eight directory inodes */
 #define ARRAY_FS_NR_DIRINODES (8)
@@ -32,6 +36,7 @@ struct arrayfs_sb {
 	struct super_block *sb;
 	spinlock_t inode_bmlock;
 	unsigned long inode_bm;
+	spinlock_t cp_lock;
 };
 
 struct arrayfs_inode {
@@ -43,32 +48,450 @@ struct arrayfs_disk_inode {
 	unsigned long size;
 };
 
+struct arrayfs_dir_entry {
+	char name[32];
+	u32 ino;
+};
+
+struct arrayfs_dir_data {
+	unsigned long bitmap;
+	struct arrayfs_dir_entry entries[64];
+};
+
+static struct inode *arrayfs_iget(struct super_block *sb, unsigned long ino);
+const struct inode_operations arrayfs_dir_iops;
+const struct inode_operations arrayfs_file_iops;
+const struct file_operations arrayfs_dir_operations;
+const struct file_operations arrayfs_file_operations;
+const struct address_space_operations arrayfs_file_aops;
+
+
 /* These are in-memory inodes. So we don't need to allocate them dynamically. */
 struct arrayfs_inode memory_inodes[ARRAYFS_NR_INODES];
 
 /* These are data storage */
 struct arrayfs_sb global_sb;
 struct arrayfs_disk_inode global_inodes[ARRAYFS_NR_INODES];
-char global_data[ARRAYFS_NR_INODES][ARRAYFS_NR_PGS_PER_FILE][4096];
+char global_data[ARRAYFS_NR_INODES][ARRAYFS_NR_PGS_PER_FILE][PAGE_SIZE];
+unsigned long disk_inode_bm;
 
 static inline struct arrayfs_inode *ARRAYFS_I(struct inode *inode)
 {
 	return container_of(inode, struct arrayfs_inode, vfs_inode);
 }
 
-static inline struct arrayfs_sb *ARRAYFS_I_SB(struct arrayfs_inode *si)
+static inline struct arrayfs_sb *ARRAYFS_I_SB(struct inode *inode)
 {
 	return &global_sb;
 }
 
-const struct inode_operations arrayfs_dir_iops = {
+static struct inode *arrayfs_new_inode(struct inode *dir, umode_t mode)
+{
+	struct arrayfs_sb *sbi = ARRAYFS_I_SB(dir);
+	unsigned long ino;
+	struct inode *inode;
+	int err;
+	struct arrayfs_disk_inode *di;
 
+	inode = new_inode(dir->i_sb);
+	if (!inode)
+		return ERR_PTR(-ENOMEM);
+
+	spin_lock(&sbi->cp_lock);
+	ino = find_first_zero_bit(&disk_inode_bm, ARRAYFS_NR_INODES);
+	if (ino == ARRAYFS_NR_INODES) {
+		spin_unlock(&sbi->cp_lock);
+		err = -ENOSPC;
+		goto fail;
+	}
+	set_bit(ino, &disk_inode_bm);
+	spin_unlock(&sbi->cp_lock);
+
+	pr_notice("%s, allocate new disk inode, pa=%lu\n",
+					__func__, ino);
+	di = &global_inodes[ino];
+	di->mode = mode;
+	di->size = 0;
+
+	inode_init_owner(inode, dir, mode);
+
+	inode->i_ino = ino;
+	inode->i_mtime = inode->i_atime = inode->i_ctime =
+			current_time(inode);
+
+	err = insert_inode_locked(inode);
+	if (err) {
+		err = -EINVAL;
+		goto failfree;
+	}
+
+	return inode;
+failfree:
+	spin_lock(&sbi->cp_lock);
+	clear_bit(ino, &disk_inode_bm);
+	spin_unlock(&sbi->cp_lock);
+fail:
+	iput(inode);
+	return ERR_PTR(err);
+}
+
+
+static int arrayfs_create(struct inode *dir, struct dentry *dentry, umode_t mode,
+						bool excl)
+{
+	struct inode *inode;
+	unsigned long ino = 0;
+	unsigned long dirino = dir->i_ino;
+	struct arrayfs_dir_data *dir_data;
+	unsigned long index;
+
+	if (dirino >= ARRAYFS_NR_INODES)
+		return -EINVAL;
+
+	//TODO: competition here
+	dir_data = (struct arrayfs_dir_data *)global_data[dirino][0];
+	index = find_first_zero_bit(&dir_data->bitmap, 64);
+	if (index == 64) {
+		pr_err("%s, not enough space for dir. ino = %lu\n",
+					__func__, dirino);
+		return -ENOSPC;
+	}
+	set_bit(index, &dir_data->bitmap);
+
+	inode = arrayfs_new_inode(dir, mode);
+	if (IS_ERR(inode))
+		return PTR_ERR(inode);
+
+	inode->i_op = &arrayfs_file_iops;
+	inode->i_fop = &arrayfs_file_operations;
+	inode->i_mapping->a_ops = &arrayfs_file_aops;
+	ino = inode->i_ino;
+
+	d_instantiate(dentry, inode);
+	unlock_new_inode(inode);
+
+	strcpy(dir_data->entries[index].name, dentry->d_name.name);
+	dir_data->entries[index].ino = ino;
+
+	return 0;
+}
+
+static int arrayfs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
+{
+	struct inode *inode;
+	unsigned long ino = 0;
+	unsigned long dirino = dir->i_ino;
+	struct arrayfs_dir_data *dir_data;
+	unsigned long index;
+
+	if (dirino >= ARRAYFS_NR_INODES)
+		return -EINVAL;
+
+	//TODO: competition here
+	dir_data = (struct arrayfs_dir_data *)global_data[dirino][0];
+	index = find_first_zero_bit(&dir_data->bitmap, 64);
+	if (index == 64) {
+		pr_err("%s, not enough space for dir. ino = %lu\n",
+					__func__, dirino);
+		return -ENOSPC;
+	}
+	set_bit(index, &dir_data->bitmap);
+
+	inode = arrayfs_new_inode(dir, mode);
+	if (IS_ERR(inode))
+		return PTR_ERR(inode);
+
+	inode->i_op = &arrayfs_dir_iops;
+	inode->i_fop = &arrayfs_dir_operations;
+	ino = inode->i_ino;
+
+	d_instantiate(dentry, inode);
+	unlock_new_inode(inode);
+
+	strcpy(dir_data->entries[index].name, dentry->d_name.name);
+	dir_data->entries[index].ino = ino;
+
+	return 0;
+}
+
+static int str_same(const char *a, const char *b)
+{
+	int i;
+
+	for (i = 0; i < 32; i++) {
+		if (a[i] != b[i])
+			return 0;
+		if (!a[i])
+			return 1;
+	}
+	return 1;
+}
+
+static struct dentry *arrayfs_lookup(struct inode *dir, struct dentry *dentry,
+		unsigned int flags)
+{
+	unsigned long dir_ino = dir->i_ino;
+	unsigned long child_ino;
+	struct arrayfs_dir_data *dirdata;
+	int index = -1;
+	struct inode *child_inode = NULL;
+	struct dentry *newdentry;
+
+	pr_notice("%s, findname=%s\n",
+				__func__, dentry->d_name.name);
+
+
+	if (dir_ino >= ARRAYFS_NR_INODES)
+		return ERR_PTR(-EINVAL);
+
+	dirdata = (struct arrayfs_dir_data *)global_data[dir_ino][0];
+
+	for (;;) {
+		index = find_next_bit(&dirdata->bitmap, 64, index + 1);
+		if (index >= 64)
+			break;
+
+		if (str_same(dirdata->entries[index].name, dentry->d_name.name)) {
+			//found
+			child_ino = dirdata->entries[index].ino;
+			child_inode = arrayfs_iget(ARRAYFS_I_SB(dir)->sb, child_ino);
+			if (IS_ERR(child_inode)) {
+				pr_err("%s, Can't get inode %lu\n",
+							__func__, child_ino);
+				return ERR_PTR(-EIO);
+			}
+			goto outSplice;
+		}
+	}
+	//not found
+outSplice:
+	newdentry = d_splice_alias(child_inode, dentry);
+	return newdentry;
+}
+
+
+const struct inode_operations arrayfs_dir_iops = {
+	.create 	= arrayfs_create,
+	.mkdir		= arrayfs_mkdir,
+	.lookup 	= arrayfs_lookup,
 };
 
 const struct inode_operations arrayfs_file_iops = {
 
 };
 
+static int arrayfs_readdir(struct file *file, struct dir_context *ctx)
+{
+	struct inode *inode = file_inode(file);
+	unsigned long ino = inode->i_ino;
+	long long pos = ctx->pos;
+	struct arrayfs_dir_data *data;
+	unsigned long index;
+	unsigned int child_ino;
+	unsigned type;
+	
+	if (ino >= ARRAYFS_NR_INODES)
+		return -EINVAL;
+
+	pr_notice("%s, pos=%lld\n",
+				__func__, pos);
+
+	data = (struct arrayfs_dir_data *)global_data[ino][0];
+	for (;;) {
+		index = find_next_bit(&data->bitmap, 64, pos);
+		if (index == 64) {
+			ctx->pos = pos = 64;
+			break;
+		} else {
+			child_ino = data->entries[index].ino;
+			if (child_ino >= ARRAYFS_NR_INODES)
+				return 1;
+			if (S_ISREG(global_inodes[child_ino].mode))
+				type = DT_REG;
+			else
+				type = DT_DIR;
+			pr_notice("%s, diremit, name[%s]\n",
+				__func__, data->entries[index].name);
+			if (!dir_emit(ctx, data->entries[index].name, strlen(data->entries[index].name),
+					child_ino, type))
+				return 1;
+			ctx->pos = pos = index + 1;
+		}
+	}
+	return 0;
+}
+
+static int arrayfs_dir_open(struct inode *inode, struct file *filp)
+{
+	pr_notice("%s\n", __func__);
+	return 0;
+}
+
+
+const struct file_operations arrayfs_dir_operations = {
+	.iterate_shared	= arrayfs_readdir,
+	.open		= arrayfs_dir_open,
+};
+
+loff_t arrayfs_file_llseek(struct file *file, loff_t offset, int whence)
+{
+	pr_notice("%s\n",
+			__func__);
+	return generic_file_llseek(file, offset, whence);
+}
+
+int arrayfs_file_open(struct inode * inode, struct file * filp)
+{
+	pr_notice("%s\n",
+			__func__);
+	return generic_file_open(inode, filp);
+}
+
+int arrayfs_file_fsync(struct file *file, loff_t start, loff_t end,
+		       int datasync)
+{
+	struct inode *inode = file->f_mapping->host;
+	int err;
+
+	pr_notice("%s\n",
+			__func__);
+
+	err = __generic_file_fsync(file, start, end, datasync);
+	if (err)
+		return err;
+	return 0;
+}
+
+
+const struct file_operations arrayfs_file_operations = {
+	.llseek		= arrayfs_file_llseek,
+	.read_iter	= generic_file_read_iter,
+	.write_iter	= generic_file_write_iter,
+	.open		= arrayfs_file_open,
+	.fsync		= arrayfs_file_fsync,
+};
+
+static int arrayfs_read_datapage(struct file *file, struct page *page)
+{
+	struct inode *inode = page->mapping->host;
+	unsigned long ino = inode->i_ino;
+	unsigned long index = page->index;
+
+	if (index >= ARRAYFS_NR_PGS_PER_FILE) {
+		pr_warning("%s, index=%lu\n",
+					__func__, index);
+		return 0;
+	}
+	
+	if (ino >= ARRAYFS_NR_INODES) {
+		pr_warning("%s, ino=%lu\n",
+					__func__, ino);
+		return 0;
+	}
+
+	memcpy(page_to_virt(page), global_data[ino][index], PAGE_SIZE);
+	SetPageUptodate(page);
+	pr_notice("%s, ino=%lu, index=%lu, pageflags=0x%lx\n",
+				__func__, ino, index, page->flags);
+	return 0;
+}
+
+
+static int arrayfs_read_data_pages(struct file *file,
+			struct address_space *mapping,
+			struct list_head *pages, unsigned nr_pages)
+{
+	unsigned page_idx;
+	gfp_t gfp = mapping_gfp_mask(mapping);
+
+	pr_notice("%s, nr_pages=%u\n",
+			__func__, nr_pages);
+
+	for (page_idx = 0; page_idx < nr_pages; page_idx++) {
+		struct page *page = lru_to_page(pages);
+
+		list_del(&page->lru);
+		if (!add_to_page_cache_lru(page, mapping, page->index, gfp))
+			arrayfs_read_datapage(file, page);
+		put_page(page);
+	}
+	return 0;
+}
+
+static int arrayfs_write_datapage(struct page *page,
+					struct writeback_control *wbc)
+{
+	struct inode *inode = page->mapping->host;
+	unsigned long index = page->index;
+	unsigned long ino = inode->i_ino;
+
+	if (index >= ARRAYFS_NR_PGS_PER_FILE) {
+		pr_warning("%s, index=%lu\n",
+					__func__, index);
+		return 0;
+	}
+	
+	if (ino >= ARRAYFS_NR_INODES) {
+		pr_warning("%s, ino=%lu\n",
+					__func__, ino);
+		return 0;
+	}
+	
+	memcpy(global_data[ino][index], page_to_virt(page), PAGE_SIZE);
+	clear_page_dirty_for_io(page);
+	pr_notice("%s, ino=%lu, index=%lu, pageflags=0x%lx\n",
+				__func__, ino, index, page->flags);
+	return 0;
+}
+
+
+static int arrayfs_write_data_pages(struct address_space *mapping,
+			    struct writeback_control *wbc)
+{
+	pgoff_t startpage = wbc->range_start >> PAGE_SHIFT;
+	pgoff_t endpage = wbc->range_end >> PAGE_SHIFT;
+	int tag = PAGECACHE_TAG_TOWRITE;
+	unsigned nrpages;
+	struct pagevec pvec;
+
+	if (endpage >= ARRAYFS_NR_PGS_PER_FILE)
+		endpage = ARRAYFS_NR_PGS_PER_FILE;
+
+	pr_notice("%s, startpage=%lu, endpage=%lu\n",
+			__func__, startpage, endpage);
+
+	pagevec_init(&pvec);
+	tag_pages_for_writeback(mapping, startpage, endpage);
+
+	while (startpage <= endpage) {
+		int i;
+
+		nrpages = pagevec_lookup_range_tag(&pvec, mapping, &startpage, endpage,
+				tag);
+		if (nrpages == 0)
+			break;
+
+		for (i = 0; i < nrpages; i++) {
+			struct page *page = pvec.pages[i];
+
+			lock_page(page);
+			arrayfs_write_datapage(page, wbc);
+			unlock_page(page);
+		}
+		pagevec_release(&pvec);
+	}
+	return 0;
+}
+
+
+const struct address_space_operations arrayfs_file_aops = {
+	.readpage	= arrayfs_read_datapage,
+	.readpages	= arrayfs_read_data_pages,
+	.writepage	= arrayfs_write_datapage,
+	.writepages	= arrayfs_write_data_pages,
+	.write_begin = simple_write_begin,
+	.write_end = simple_write_end,
+};
 
 static struct inode *arrayfs_alloc_inode(struct super_block *sb)
 {
@@ -98,7 +521,7 @@ static void arrayfs_destroy_inode(struct inode *inode)
 {
 	struct arrayfs_inode *si = ARRAYFS_I(inode);
 	int pa = si - memory_inodes;
-	//struct arrayfs_sb *sbi = ARRAYFS_I_SB(si);
+	//struct arrayfs_sb *sbi = ARRAYFS_I_SB(inode);
 
 	pr_notice("%s, %d\n", __func__, pa);
 
@@ -158,8 +581,11 @@ static struct inode *arrayfs_iget(struct super_block *sb, unsigned long ino)
 
 	if (S_ISREG(inode->i_mode)) {
 		inode->i_op = &arrayfs_file_iops;
+		inode->i_fop = &arrayfs_file_operations;
+		inode->i_mapping->a_ops = &arrayfs_file_aops;
 	} else if (S_ISDIR(inode->i_mode)) {
 		inode->i_op = &arrayfs_dir_iops;
+		inode->i_fop = &arrayfs_dir_operations;
 	}
 	unlock_new_inode(inode);
 	return inode;
@@ -188,6 +614,7 @@ static int arrayfs_fill_super(struct super_block *sb, void *data, int silent)
 	sbi->sb = sb;
 	sbi->inode_bm = 0;
 	spin_lock_init(&sbi->inode_bmlock);
+	spin_lock_init(&sbi->cp_lock);
 	sb->s_op = &arrayfs_sops;
 
 	/* Deal with root inode */
@@ -239,9 +666,13 @@ MODULE_ALIAS_FS("arrayfs");
 static void mkfs_arrayfs(void)
 {
 	struct arrayfs_disk_inode *di = &global_inodes[0];
+	struct arrayfs_dir_data *dd = (struct arrayfs_dir_data *)global_data[0][0];
 
-	di->mode = S_IFDIR;
+	di->mode = S_IFDIR | S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
 	di->size = 0;
+	disk_inode_bm = 0;
+	set_bit(0, &disk_inode_bm);
+	dd->bitmap = 0;
 }
 
 static int __init init_arrayfs(void)
@@ -273,6 +704,6 @@ module_exit(exit_arrayfs)
 
 
 MODULE_AUTHOR("Hongwei Qin <glqhw@hust.edu.cn>");
-MODULE_LICENSE("MIT");
+MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Array File System");
 
